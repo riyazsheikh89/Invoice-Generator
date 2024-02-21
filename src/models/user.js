e0724551a53b674ad0ed050a65d03670b38c5fb1
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2024-02-21 23:41:14 +0530

    added logic for encrypted password, generateToken

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -15,6 +17,31 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, {timestamps: true});
+
+// encrypt the password before saving the model
+userSchema.pre("save", function(next) {
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
+    next();
+});
+
+// Compare the password
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+    // returns true, if the password matches
+}
+
+// Generate JWT token for authetication purpose
+userSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+        {id: this._id, email: this.email},  // this info is used to create the JWT
+        process.env.JWT_KEY,                // secrect key: to sign the JWT
+        {expiresIn: process.env.JWT_EXPIRES_IN} // JWT expiry time
+    );
+    return token;
+}
 
 // Creating user model
 const User = mongoose.model('User', userSchema);
