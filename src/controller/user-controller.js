c53424e5aec75f121d80257143279a0370c0a8f3
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2024-02-22 00:36:19 +0530

    added login route with logic

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -4,10 +4,8 @@ import User from "../models/user.js"
 const signup = async (req, res) => {
     try {
         const isValidData = await validateInputData(req.body);
-        if (isValidData !== "success") {
-            return res.status(422).json({
-                message: isValidData
-            });
+        if (isValidData !== "success") {    // if input data is not valid, 
+            throw isValidData;              // throw the corresponding error
         } else {
             const { name, email, password } = req.body;
             const user = await User.create({ name, email, password });
@@ -19,15 +17,41 @@ const signup = async (req, res) => {
             });
         }
     } catch (error) {
+        const err = (error.code === 11000) ? "Email already exist!" : error
         return res.status(500).json({
             success: false,
             data: {},
             message: 'Something went wrong in registration!',
-            err: error
+            err
         });
     }
 }
 
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !user.comparePassword(password)) {
+            throw "Incorrect email or password! please check your credentials";
+        } else {
+            const auth_token = user.generateToken(); // will generate the JWT
+            return res.status(201).json({
+                success: true,
+                data: auth_token,
+                message: "Successfully logged into your account",
+                err: {}
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: {},
+            message: 'Oops! Something went wrong with login',
+            err: error
+        });
+    }
+}
 
 // Helper function for input validation
 async function validateInputData(inputData) {
@@ -52,4 +76,5 @@ async function validateInputData(inputData) {
 
 export {
     signup,
+    login,
 }
