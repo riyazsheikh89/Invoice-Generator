c44238b8ce271f5e6ca0657a2607141e4f3c8a93
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2024-02-22 10:22:18 +0530

    updated authentication middleware

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,6 +3,9 @@ import jwt from "jsonwebtoken";
 export const authenticate = (req, res, next) => {
     try {
         const { authorization } = req.headers;
+        if (!authorization) {
+            throw "No authentication token found!";
+        }
         // TOKEN will be sent inside req authorization headers as bearer
         const token = authorization.split(" ")[1];
         const decoodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
